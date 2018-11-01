92472a22a00b75e364945f28a32b027c9bef4e80
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-11-01 16:07:15 +0800

    Added 2 way data binding on email field and control the sign in/sign up buttons

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,4 +5,15 @@ import { Component } from "@angular/core";
   templateUrl: "app.html",
   styleUrls: ['pages/login/login-common.css', 'pages/login/login.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  email = 'alex@nuvious.com';
+  isLoggingIn = true;
+
+  submit(e) {
+    console.log(`[DEBUG] - <AppComponent.submit> email: ${this.email}`);
+  }
+
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 import { AppComponent } from "./app.component";
 // import { LoginComponent } from "./pages"; 
 
 @NgModule({
-  imports: [NativeScriptModule],
+  imports: [NativeScriptModule, NativeScriptFormsModule],
   declarations: [AppComponent],
     // , LoginComponent],
   bootstrap: [AppComponent],
